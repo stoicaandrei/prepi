92ed04bd2e7ef246037ffa50bd07b434a97e8167
Andrei Stoica <andrei@stoica.dev>
2024-09-04 16:38:07 +0300

    Add tailwind

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -6,17 +6,7 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
