6134d558b3628bf6e551fd81a3ad82ba2f04576f
Andrei Stoica <andrei@stoica.dev>
2024-09-06 05:41:31 +0300

    Short demo changes

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,7 +1,7 @@
 export async function getServerSideProps() {
   return {
     redirect: {
-      destination: "/exams",
+      destination: "/dashboard",
       permanent: false, // If true, the redirect is cached permanently
     },
   };
