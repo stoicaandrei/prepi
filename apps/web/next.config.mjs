19546423032681be76cae1c6dc5637c2cbc62aa2
Andrei Stoica <andrei@stoica.dev>
2024-09-10 12:14:48 +0300

    Fix schema path for nextjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -3,7 +3,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-// !process.env.SKIP_ENV_VALIDATION && (await import("./env/server.mjs"));
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
 
 const nextConfig = {
   reactStrictMode: true,
@@ -13,6 +13,13 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: !!process.env.CI,
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  }
 };
 
 export default nextConfig;
