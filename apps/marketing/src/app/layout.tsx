7edd378ca8fd2f70ce6b5eee028aae9068985580
Andrei Stoica <andrei@stoica.dev>
2024-09-16 13:41:03 +0300

    Fix favicon

--- apps/marketing/src/app/layout.tsx
+++ apps/marketing/src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <title>Prepi</title>
       </Head>
       <body className="min-h-screen flex flex-col">
