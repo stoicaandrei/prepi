1b455d0a7975e19aeb34f7430504dbbc8a4e619d
Andrei Stoica <andrei@stoica.dev>
2024-09-20 15:40:08 +0300

    Update price

--- apps/marketing/src/constants.ts
+++ apps/marketing/src/constants.ts
@@ -1,3 +1,3 @@
 // export const startNowUrl = "https://app.prepi.ro";
 export const startNowUrl = "/coming-soon";
-export const currentPrice = "84.99 lei/lună";
+export const currentPrice = "89 lei/lună";
