6a1d1452a87e79c36948b4685da40089b06e3d99
Andrei Stoica <andrei@stoica.dev>
2024-09-22 15:20:41 +0300

    Add reactStrictMode and swcMinify to next.config.mjs

--- apps/marketing/next.config.mjs
+++ apps/marketing/next.config.mjs
@@ -2,6 +2,8 @@ import createMDX from '@next/mdx'
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
