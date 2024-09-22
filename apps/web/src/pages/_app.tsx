ca04b08eb56895b44f10c7011bbfa7ae073df52c
Andrei Stoica <andrei@stoica.dev>
2024-09-22 03:22:58 +0300

    Add posthog to web

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -7,20 +7,36 @@ import { Layout } from "@/components/layouts/MainLayout";
 import { MathJaxContext } from "better-react-mathjax";
 import { Analytics } from "@vercel/analytics/react";
 import Head from "next/head";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+if (typeof window !== "undefined") {
+  // checks that we are client-side
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "", {
+    api_host:
+      process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com",
+    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
+    },
+  });
+}
 
 const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   return (
-    <ClerkProvider {...pageProps}>
-      <MathJaxContext>
-        <Head>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Layout>
-          <Component {...pageProps} />
-          <Analytics />
-        </Layout>
-      </MathJaxContext>
-    </ClerkProvider>
+    <PostHogProvider client={posthog}>
+      <ClerkProvider>
+        <MathJaxContext>
+          <Head>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <Layout>
+            <Component {...pageProps} />
+            <Analytics />
+          </Layout>
+        </MathJaxContext>
+      </ClerkProvider>
+    </PostHogProvider>
   );
 };
 
