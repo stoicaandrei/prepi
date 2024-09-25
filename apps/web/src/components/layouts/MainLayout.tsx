a90b1f187aaef967cc20e699eb71a1c3ba9f2f57
Andrei Stoica <andrei@stoica.dev>
2024-09-25 18:32:53 +0300

    Fix build

--- apps/web/src/components/layouts/MainLayout.tsx
+++ apps/web/src/components/layouts/MainLayout.tsx
@@ -168,176 +168,176 @@ export function Layout({ children }: LayoutProps) {
     </div>
   );
 
-  return (
-    <div className="grid min-h-screen w-full md:grid-cols-[315px_1fr] lg:grid-cols-[315px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
-        <div className="flex h-full max-h-screen flex-col gap-2">
-          <div className="flex h-14 items-center border-b lg:h-[80px]">
-            {/* <Link href="/"> */}
-            <Image
-              src="/logo.svg"
-              alt="Prepi logo"
-              width={100}
-              height={32}
-              className="flex-shrink-0"
-            />
-            {/* </Link> */}
-          </div>
-          <div className="flex-1">
-            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-              <Link
-                href="/dashboard"
-                className={navClasses(activeSection === "dashboard")}
-              >
-                <ChartPie className="h-5 w-5 flex-shrink-0" />
-                Dashboard
-              </Link>
-              <Link
-                href="/lessons"
-                className={navClasses(activeSection === "lessons")}
-              >
-                <BookOpen className="h-4 w-4" />
-                Lecții
-              </Link>
-              <Link
-                href="/practice"
-                className={navClasses(activeSection === "practice")}
-              >
-                <ListChecks className="h-4 w-4" />
-                Exersează
-              </Link>
-              <Link
-                href="/exams"
-                className={navClasses(activeSection === "exams")}
-              >
-                <Files className="h-4 w-4" />
-                Variante
-              </Link>
-              <Link
-                href="/leaderboard"
-                className={navClasses(activeSection === "leaderboard")}
-              >
-                <Star className="h-4 w-4" />
-                Clasament
-              </Link>
-              <Link
-                href="/store"
-                className={navClasses(activeSection === "store")}
-              >
-                <ShoppingBag className="h-4 w-4" />
-                Magazin
-              </Link>
-            </nav>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-col">
-        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button
-                variant="outline"
-                size="icon"
-                className="shrink-0 md:hidden"
-              >
-                <Menu className="h-5 w-5" />
-                <span className="sr-only">Toggle navigation menu</span>
-              </Button>
-            </SheetTrigger>
-            <SheetContent side="left" className="flex flex-col">
-              <nav className="grid gap-2 text-lg font-medium">
-                <Link
-                  href="#"
-                  className="flex items-center gap-2 text-lg font-semibold"
-                >
-                  <Book className="h-6 w-6" />
-                  <span className="">Prepi</span>
-                </Link>
-                <Link
-                  href="/dashboard"
-                  className={navClasses(activeSection === "dashboard", true)}
-                >
-                  <ChartPie className="h-4 w-4" />
-                  Dashboard
-                </Link>
-                <Link
-                  href="/lessons"
-                  className={navClasses(activeSection === "lessons", true)}
-                >
-                  <BookOpen className="h-4 w-4" />
-                  Lecții
-                </Link>
-                <Link
-                  href="/practice"
-                  className={navClasses(activeSection === "practice", true)}
-                >
-                  <ListChecks className="h-4 w-4" />
-                  Exersează
-                </Link>
-                <Link
-                  href="/exams"
-                  className={navClasses(activeSection === "exams", true)}
-                >
-                  <Files className="h-4 w-4" />
-                  Variante
-                </Link>
-                <Link
-                  href="/leaderboard"
-                  className={navClasses(activeSection === "leaderboard", true)}
-                >
-                  <Star className="h-4 w-4" />
-                  Clasament
-                </Link>
-                <Link
-                  href="/store"
-                  className={navClasses(activeSection === "store", true)}
-                >
-                  <ShoppingBag className="h-4 w-4" />
-                  Magazin
-                </Link>
-              </nav>
-            </SheetContent>
-          </Sheet>
-          <div className="w-full flex-1 empty-space"></div>
-          <div className="flex items-center bg-cyan-100 text-cyan-700 rounded-full px-4 py-1">
-            <Flame className="w-4 h-4 mr-2" />
-            <span className="text-sm font-medium">
-              0 <span className="hidden sm:inline">zile</span>
-            </span>
-          </div>
-          <div className="flex items-center bg-blue-100 text-blue-700 rounded-full px-4 py-1">
-            <Coins className="w-4 h-4 mr-2" />
-            <span className="text-sm font-medium">
-              0 <span className="hidden sm:inline">puncte</span>
-            </span>
-          </div>
-          <div className="flex items-center">
-            <div className="text-right mr-4 hidden sm:inline">
-              <h1 className="text-xl font-bold">Salut, Andreieii!</h1>
-              <p className="text-sm text-gray-600">Învățăcel</p>
-            </div>
-          </div>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="secondary" size="icon" className="rounded-full">
-                <CircleUser className="h-5 w-5" />
-                <span className="sr-only">Toggle user menu</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuItem disabled>Settings</DropdownMenuItem>
-              <DropdownMenuItem disabled>Support</DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem onClick={() => signOut()}>
-                Logout
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          {children}
-        </main>
-      </div>
-    </div>
-  );
+  // return (
+  //   <div className="grid min-h-screen w-full md:grid-cols-[315px_1fr] lg:grid-cols-[315px_1fr]">
+  //     <div className="hidden border-r bg-muted/40 md:block">
+  //       <div className="flex h-full max-h-screen flex-col gap-2">
+  //         <div className="flex h-14 items-center border-b lg:h-[80px]">
+  //           {/* <Link href="/"> */}
+  //           <Image
+  //             src="/logo.svg"
+  //             alt="Prepi logo"
+  //             width={100}
+  //             height={32}
+  //             className="flex-shrink-0"
+  //           />
+  //           {/* </Link> */}
+  //         </div>
+  //         <div className="flex-1">
+  //           <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+  //             <Link
+  //               href="/dashboard"
+  //               className={navClasses(activeSection === "dashboard")}
+  //             >
+  //               <ChartPie className="h-5 w-5 flex-shrink-0" />
+  //               Dashboard
+  //             </Link>
+  //             <Link
+  //               href="/lessons"
+  //               className={navClasses(activeSection === "lessons")}
+  //             >
+  //               <BookOpen className="h-4 w-4" />
+  //               Lecții
+  //             </Link>
+  //             <Link
+  //               href="/practice"
+  //               className={navClasses(activeSection === "practice")}
+  //             >
+  //               <ListChecks className="h-4 w-4" />
+  //               Exersează
+  //             </Link>
+  //             <Link
+  //               href="/exams"
+  //               className={navClasses(activeSection === "exams")}
+  //             >
+  //               <Files className="h-4 w-4" />
+  //               Variante
+  //             </Link>
+  //             <Link
+  //               href="/leaderboard"
+  //               className={navClasses(activeSection === "leaderboard")}
+  //             >
+  //               <Star className="h-4 w-4" />
+  //               Clasament
+  //             </Link>
+  //             <Link
+  //               href="/store"
+  //               className={navClasses(activeSection === "store")}
+  //             >
+  //               <ShoppingBag className="h-4 w-4" />
+  //               Magazin
+  //             </Link>
+  //           </nav>
+  //         </div>
+  //       </div>
+  //     </div>
+  //     <div className="flex flex-col">
+  //       <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+  //         <Sheet>
+  //           <SheetTrigger asChild>
+  //             <Button
+  //               variant="outline"
+  //               size="icon"
+  //               className="shrink-0 md:hidden"
+  //             >
+  //               <Menu className="h-5 w-5" />
+  //               <span className="sr-only">Toggle navigation menu</span>
+  //             </Button>
+  //           </SheetTrigger>
+  //           <SheetContent side="left" className="flex flex-col">
+  //             <nav className="grid gap-2 text-lg font-medium">
+  //               <Link
+  //                 href="#"
+  //                 className="flex items-center gap-2 text-lg font-semibold"
+  //               >
+  //                 <Book className="h-6 w-6" />
+  //                 <span className="">Prepi</span>
+  //               </Link>
+  //               <Link
+  //                 href="/dashboard"
+  //                 className={navClasses(activeSection === "dashboard", true)}
+  //               >
+  //                 <ChartPie className="h-4 w-4" />
+  //                 Dashboard
+  //               </Link>
+  //               <Link
+  //                 href="/lessons"
+  //                 className={navClasses(activeSection === "lessons", true)}
+  //               >
+  //                 <BookOpen className="h-4 w-4" />
+  //                 Lecții
+  //               </Link>
+  //               <Link
+  //                 href="/practice"
+  //                 className={navClasses(activeSection === "practice", true)}
+  //               >
+  //                 <ListChecks className="h-4 w-4" />
+  //                 Exersează
+  //               </Link>
+  //               <Link
+  //                 href="/exams"
+  //                 className={navClasses(activeSection === "exams", true)}
+  //               >
+  //                 <Files className="h-4 w-4" />
+  //                 Variante
+  //               </Link>
+  //               <Link
+  //                 href="/leaderboard"
+  //                 className={navClasses(activeSection === "leaderboard", true)}
+  //               >
+  //                 <Star className="h-4 w-4" />
+  //                 Clasament
+  //               </Link>
+  //               <Link
+  //                 href="/store"
+  //                 className={navClasses(activeSection === "store", true)}
+  //               >
+  //                 <ShoppingBag className="h-4 w-4" />
+  //                 Magazin
+  //               </Link>
+  //             </nav>
+  //           </SheetContent>
+  //         </Sheet>
+  //         <div className="w-full flex-1 empty-space"></div>
+  //         <div className="flex items-center bg-cyan-100 text-cyan-700 rounded-full px-4 py-1">
+  //           <Flame className="w-4 h-4 mr-2" />
+  //           <span className="text-sm font-medium">
+  //             0 <span className="hidden sm:inline">zile</span>
+  //           </span>
+  //         </div>
+  //         <div className="flex items-center bg-blue-100 text-blue-700 rounded-full px-4 py-1">
+  //           <Coins className="w-4 h-4 mr-2" />
+  //           <span className="text-sm font-medium">
+  //             0 <span className="hidden sm:inline">puncte</span>
+  //           </span>
+  //         </div>
+  //         <div className="flex items-center">
+  //           <div className="text-right mr-4 hidden sm:inline">
+  //             <h1 className="text-xl font-bold">Salut, Andreieii!</h1>
+  //             <p className="text-sm text-gray-600">Învățăcel</p>
+  //           </div>
+  //         </div>
+  //         <DropdownMenu>
+  //           <DropdownMenuTrigger asChild>
+  //             <Button variant="secondary" size="icon" className="rounded-full">
+  //               <CircleUser className="h-5 w-5" />
+  //               <span className="sr-only">Toggle user menu</span>
+  //             </Button>
+  //           </DropdownMenuTrigger>
+  //           <DropdownMenuContent align="end">
+  //             <DropdownMenuItem disabled>Settings</DropdownMenuItem>
+  //             <DropdownMenuItem disabled>Support</DropdownMenuItem>
+  //             <DropdownMenuSeparator />
+  //             <DropdownMenuItem onClick={() => signOut()}>
+  //               Logout
+  //             </DropdownMenuItem>
+  //           </DropdownMenuContent>
+  //         </DropdownMenu>
+  //       </header>
+  //       <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+  //         {children}
+  //       </main>
+  //     </div>
+  //   </div>
+  // );
 }
