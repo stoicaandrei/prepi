1475f1f1bed401b601d23a58e8eefeab86592beb
Andrei Stoica <andrei@stoica.dev>
2024-10-03 01:20:04 +0300

    kv errror fix?

--- packages/api/src/env.ts
+++ packages/api/src/env.ts
@@ -5,15 +5,15 @@ import { z } from "zod";
 dotenv.config();
 
 // Define a schema for environment variables
-const envSchema = z.object({
-  KV_URL: z.string().url(),
-  KV_REST_API_URL: z.string().min(1),
-});
+// const envSchema = z.object({
+//   KV_URL: z.string().url(),
+//   KV_REST_API_URL: z.string().min(1),
+// });
 
-// Validate and extract environment
-const env = envSchema.parse(process.env);
+// // Validate and extract environment
+// const env = envSchema.parse(process.env);
 
 export const config = {
-  KV_URL: env.KV_URL,
-  KV_REST_API_URL: env.KV_REST_API_URL,
+  KV_URL: process.env.KV_URL,
+  KV_REST_API_URL: process.env.KV_REST_API_URL,
 };
