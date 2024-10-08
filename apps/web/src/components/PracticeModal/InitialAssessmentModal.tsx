7d00bfb5b540035db1f06aaffcce85bff182c374
Andrei Stoica <andrei@stoica.dev>
2024-10-08 04:12:07 +0300

    Refactor InitialAssessmentModal component

--- apps/web/src/components/PracticeModal/InitialAssessmentModal.tsx
+++ apps/web/src/components/PracticeModal/InitialAssessmentModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { trpc } from "@/utils/trpc";
 import { SubmissionStatus } from "./ProblemsProgress";
@@ -80,6 +80,22 @@ export function InitialAssessmentModal({
     sendAttemptRecording(false);
   };
 
+  const DialogWrapper = useCallback(
+    ({ children }: { children: React.ReactNode }) => {
+      return (
+        <Dialog open={open} onOpenChange={onClose}>
+          <DialogContent
+            onClose={onClose}
+            className="max-w-full w-[90%] min-h-[90vh] md:min-h-[50vh] overflow-scroll"
+          >
+            <div className="p-6">{children}</div>
+          </DialogContent>
+        </Dialog>
+      );
+    },
+    [open, onClose],
+  );
+
   if (problemLoading || recordAssessmentQuestion.isLoading) {
     let message = "Se pregătește următoarea problemă...";
     if (recordAssessmentQuestion.isLoading) {
@@ -87,83 +103,66 @@ export function InitialAssessmentModal({
     }
 
     return (
-      <Dialog open={open} onOpenChange={() => {}}>
-        <DialogContent
-          onClose={onClose}
-          className="max-w-full w-[90%] min-h-[90vh] md:min-h-[50vh] overflow-scroll"
-        >
-          <div className="p-6">
-            <ModalLoader message={message} />
-          </div>
-        </DialogContent>
-      </Dialog>
+      <DialogWrapper>
+        <ModalLoader message={message} />
+      </DialogWrapper>
     );
   }
 
   return (
-    <Dialog open={open} onOpenChange={() => {}}>
-      <DialogContent
-        onClose={onClose}
-        className="max-w-full w-[90%] min-h-[90vh] md:min-h-[50vh] overflow-scroll"
-      >
-        <div className="p-6">
-          <div className="flex justify-between items-center mb-6 flex-wrap gap-6">
-            <h2 className="text-2xl font-bold">Test inițial</h2>
-            <div className="flex flex-row gap-2 items-center">
-              <span>
-                {assessedQuestions} / {totalQuestions}
-              </span>
-              <Progress
-                className="shadow-prepi h-4 md:w-96"
-                value={progress * 100}
-              />
-            </div>
-          </div>
-
-          <div className="mb-6">
-            {problem && (
-              <ProblemDisplay
-                problem={problem}
-                answerAttempt={answerAttempt}
-                setAnswerAttempt={setAnswerAttempt}
-                hideInput={false}
-                hintCount={0}
-                showExplanation={false}
-              />
-            )}
-          </div>
-
-          <div className="flex flex-col md:flex-row md:justify-end items-start md:items-center gap-2 md:gap-2">
-            <Button variant="outline" className="mr-2" onClick={skipProblem}>
-              Sari peste
-            </Button>
-
-            <Button onClick={submitAnswer}>
-              Trimite <ChevronRight className="ml-2 h-4 w-4" />
-            </Button>
-          </div>
-
-          {isTester && (
-            <div className="">
-              <Button variant="link" onClick={() => sendAttemptRecording(true)}>
-                Click Correct Answer
-              </Button>
-              <Button
-                variant="link"
-                onClick={() => sendAttemptRecording(false)}
-              >
-                Click Wrong Answer
-              </Button>
-              <Button
-                variant="link"
-                onClick={() => navigator.clipboard.writeText(problem?.id ?? "")}
-              >
-                {problem?.id}
-              </Button>
-            </div>
-          )}
+    <DialogWrapper>
+      <div className="flex justify-between items-center mb-6 flex-wrap gap-6">
+        <h2 className="text-2xl font-bold">Test inițial</h2>
+        <div className="flex flex-row gap-2 items-center">
+          <span>
+            {assessedQuestions} / {totalQuestions}
+          </span>
+          <Progress
+            className="shadow-prepi h-4 md:w-96"
+            value={progress * 100}
+          />
         </div>
-      </DialogContent>
-    </Dialog>
+      </div>
+
+      <div className="mb-6">
+        {problem && (
+          <ProblemDisplay
+            problem={problem}
+            answerAttempt={answerAttempt}
+            setAnswerAttempt={setAnswerAttempt}
+            hideInput={false}
+            hintCount={0}
+            showExplanation={false}
+          />
+        )}
+      </div>
+
+      <div className="flex flex-col md:flex-row md:justify-end items-start md:items-center gap-2 md:gap-2">
+        <Button variant="outline" className="mr-2" onClick={skipProblem}>
+          Sari peste
+        </Button>
+
+        <Button onClick={submitAnswer}>
+          Trimite <ChevronRight className="ml-2 h-4 w-4" />
+        </Button>
+      </div>
+
+      {isTester && (
+        <div className="">
+          <Button variant="link" onClick={() => sendAttemptRecording(true)}>
+            Click Correct Answer
+          </Button>
+          <Button variant="link" onClick={() => sendAttemptRecording(false)}>
+            Click Wrong Answer
+          </Button>
+          <Button
+            variant="link"
+            onClick={() => navigator.clipboard.writeText(problem?.id ?? "")}
+          >
+            {problem?.id}
+          </Button>
+        </div>
+      )}
+    </DialogWrapper>
   );
 }
