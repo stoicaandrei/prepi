79c68a78498607d8033b138551cb18c4448399f7
Andrei Stoica <andrei@stoica.dev>
2024-10-09 13:19:43 +0300

    Change html lang in marketing website

--- apps/marketing/src/app/layout.tsx
+++ apps/marketing/src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ro">
       <Providers>
         <Head>
           <link rel="icon" href="/favicon.ico" sizes="any" />
