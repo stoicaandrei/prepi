d54e49469297026bb66ef84bc88a57376659e6b2
Andrei Stoica <andrei@stoica.dev>
2024-10-15 17:26:43 +0300

    Install sentry

---
+++ apps/web/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://4df716377ce645d574f988589e236026@o4508127047778304.ingest.de.sentry.io/4508127049220176",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
